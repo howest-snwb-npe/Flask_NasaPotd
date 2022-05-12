de6f5fe8bc9556de83c4803c4e8186df0a6d3bf7
koen-db <koen.de.bruyne@howest.be>
2022-05-12 13:54:05 +0200

    Add API-call and raw response to html

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from crypt import methods
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -6,10 +8,14 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
-@app.route("/search")
+@app.route("/search",methods=["POST"])
 def search():
-    quantity = request.args.get("quantity")
-    return render_template("search.html",quantity=quantity)
+    quantity = request.form.get("quantity")
+    url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5"
+    response = requests.get(url)
+    print(response.json())
+
+    return render_template("search.html",quantity=quantity,response=response.json())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
