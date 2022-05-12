73ef75075e7a609083d0b887229e49582023e434
koen-db <koen.de.bruyne@howest.be>
2022-05-12 13:35:11 +0200

    Add form to page

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     return render_template("index.html")
+
+@app.route("/search")
+def search():
+    quantity = request.args.get("quantity")
+    return render_template("search.html",quantity=quantity)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
